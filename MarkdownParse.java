121ca11363fe2032829b8cf7e771af3ab599b7f3
dmontefalcon <dmontefalcon@ucsd.edu>
2022-04-15 11:06:54 -0700

    debugged separated links

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -19,8 +19,12 @@ public class MarkdownParse {
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
+            if (closeBracket == openParen - 1) {
+                toReturn.add(markdown.substring(openParen + 1, closeParen));
+            }
             currentIndex = closeParen + 1;
+
+            
         }
 
         return toReturn;
