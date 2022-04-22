5e7ca9fcebfc43fcf083bb009dba36b060321afc
dmontefalcon <dmontefalcon@ucsd.edu>
2022-04-22 10:20:33 -0700

    junit updated

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -40,6 +40,7 @@ public class MarkdownParse {
 
     public static void main(String[] args) throws IOException {
         Path fileName = Path.of(args[0]);
+        System.out.println("HELLO REMEMBER TO REMOVE ME!");
         String content = Files.readString(fileName);
         ArrayList<String> links = getLinks(content);
 	    System.out.println(links);
